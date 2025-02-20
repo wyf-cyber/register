82e4a413cd1f37b1a0a0384f4c7d531936f51254
wyf-cyber <wyf17326280236@gmail.com>
2025-02-20 10:29:56 +0800

    add captcha generation and account setting ability

---
+++ src/main/java/com/itheima/controller/webToolsController.java
@@ -0,0 +1,23 @@
+package com.itheima.controller;
+
+import com.itheima.service.webToolsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tools")
+public class webToolsController {
+
+    @Autowired
+    private webToolsService webToolsService;
+
+    @GetMapping("/Captcha")
+    public ResponseEntity<?> generateCaptcha() {
+        return webToolsService.getCaptcha();
+    }
+}
